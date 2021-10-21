1a1cd5a823e6ac8747ece6da3750f2173038fa61
cwelland1 <75807390+cwelland1@users.noreply.github.com>
2021-10-21 16:11:05 -0400

    Update DockerHelloWorld.java

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -3,15 +3,14 @@ package com.javaexpress.docker.dockerspringboothello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RestController
 @RequestMapping("/v1/docker")
-@SpringBootApplication
 public class DockerHelloWorld {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerHelloWorld.class, args);
+	@GetMapping
+	public String getName() {
+
+		return "Welcome to Java Express Channel";
 	}
 }
