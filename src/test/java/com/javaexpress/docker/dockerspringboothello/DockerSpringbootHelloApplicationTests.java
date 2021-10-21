71594069bca04dd798632afb2f83c6f4ca23dce5
cwelland1 <75807390+cwelland1@users.noreply.github.com>
2021-10-21 15:39:02 -0400

    Update DockerSpringbootHelloApplicationTests.java

--- src/test/java/com/javaexpress/docker/dockerspringboothello/DockerSpringbootHelloApplicationTests.java
+++ src/test/java/com/javaexpress/docker/dockerspringboothello/DockerSpringbootHelloApplicationTests.java
@@ -1,13 +1,16 @@
 package com.javaexpress.docker.dockerspringboothello;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DockerSpringbootHelloApplicationTests {
+public class DockerSpringbootHelloApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
